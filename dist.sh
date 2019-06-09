faa33da84cf4ccb2158067051c39831be212f1b6
Jia Jia <jia.jia@zte.com.cn>
2019-06-09 16:40:52 +0800

    Set version to 0.0.2

--- dist.sh
+++ dist.sh
@@ -5,5 +5,7 @@ pip3 install -U twine
 pip3 install -U wheel
 pip3 install -Ur requirements.txt
 
+rm -rf buid dist mailtrigger.egg-info/
+
 python3 setup.py sdist bdist_wheel
 python3 -m twine upload dist/*
