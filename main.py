bfa1fba84aa0ebb0a6bbd1922dbcebd70a769569
Jia Jia <angersax@sina.com>
2019-06-15 00:24:48 +0800

    Invoke trigger in main routine

--- main.py
+++ main.py
@@ -19,25 +19,78 @@ SCHEDULER = 'mailtrigger/config/scheduler.json'
 TRIGGER = 'mailtrigger/config/trigger.json'
 
 
-def run_job(args):
-    def _run_receiver(receiver):
-        receiver.connect()
-        data = receiver.retrieve()
-        receiver.disconnect()
-        return data
+def _format(data, content):
+    return {
+        'content': '\n'.join((
+            content,
+            '%s' % ('-'*80),
+            '> From: %s' % data['from'],
+            '> To: %s' % data['to'],
+            '> Subject: %s' % data['subject'],
+            '> Date: %s' % data['date'],
+            '>',
+            '> Content: %s' % data['content'])),
+        'date': time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time())),
+        'from': data['to'],
+        'subject': 'Re: %s' % data['subject'],
+        'to': data['from']
+    }
 
-    def _run_trigger(data, sender, triggers):
-        """TODO"""
-        pass
 
+def _emit(data, sender, triggers):
+    """TODO"""
+
+
+def _help(data, sender, triggers):
+    def _run_help(name):
+        cls = None
+        for item in REGISTRY:
+            if item['name'] == name:
+                cls = item['class']
+                break
+        return cls
+    buf = []
+    for item in triggers:
+        cls = _run_help(item)
+        if cls is not None:
+            buf.append(cls.help())
+    sender.send(format(data, os.linesep.join(buf)))
+
+
+def _trigger(data, sender, triggers):
+    for item in data:
+        t = item.split()[0].lstrip('@').strip()
+        if t == 'help':
+            _help(data, sender, triggers)
+        else:
+            _emit(item, sender, triggers)
+
+
+def _unpack(data):
+    buf = []
+    lines = data['content'].splitlines()
+
+    for item in lines:
+        if len(item.strip()) != 0:
+            data['content'] = item.strip()
+            buf.append(data)
+
+    return buf
+
+
+def _retrieve(receiver):
+    return receiver.retrieve()
+
+
+def _job(args):
     receiver, sender, triggers = args
-
-    data = _run_receiver(receiver)
-    _run_trigger(data, sender, triggers)
+    data = _retrieve(receiver)
+    data = _unpack(data)
+    _trigger(data, sender, triggers)
 
 
 def run_scheduler(sched, receiver, sender, triggers):
-    sched.add(run_job, [receiver, sender, triggers], 'run_job')
+    sched.add(_job, [receiver, sender, triggers], '_job')
 
     while True:
         sched.run()
@@ -62,11 +115,18 @@ def main():
         Logger.error(str(e))
         return -2
 
+    receiver = None
+    sender = None
+
     try:
         receiver = Receiver(os.path.join(os.path.dirname(__file__), MAILER))
+        receiver.connect()
         sender = Sender(os.path.join(os.path.dirname(__file__), MAILER))
+        sender.connect()
     except (ReceiverException, SenderException) as e:
         Logger.error(str(e))
+        sender.disconnect()
+        receiver.disconnect()
         sched.stop()
         return -3
 
