b306478753bbe09ef125b7ea597ada2d5ac3b0c2
Jia Jia <angersax@sina.com>
2019-06-15 20:59:40 +0800

    Add timestamp in logger

--- mailtrigger/logger/logger.py
+++ mailtrigger/logger/logger.py
@@ -2,6 +2,7 @@
 
 import colorama
 import sys
+import time
 
 
 class Logger(object):
@@ -10,28 +11,32 @@ class Logger(object):
 
     @staticmethod
     def debug(msg):
-        sys.stderr.write(u'{debug}DEBUG:{reset} {msg}\n'.format(
+        sys.stderr.write(u'{debug}{time} DEBUG:{reset} {msg}\n'.format(
             debug=colorama.Fore.GREEN + colorama.Style.BRIGHT,
+            time=time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time())),
             reset=colorama.Style.RESET_ALL,
             msg=msg))
 
     @staticmethod
     def error(msg):
-        sys.stderr.write(u'{error}ERROR:{reset} {msg}\n'.format(
+        sys.stderr.write(u'{error}{time} ERROR:{reset} {msg}\n'.format(
             error=colorama.Fore.RED + colorama.Style.BRIGHT,
+            time=time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time())),
             reset=colorama.Style.RESET_ALL,
             msg=msg))
 
     @staticmethod
     def info(msg):
-        sys.stderr.write(u'{info}info:{reset} {msg}\n'.format(
+        sys.stderr.write(u'{info}{time} INFO:{reset} {msg}\n'.format(
             info=colorama.Fore.WHITE + colorama.Style.BRIGHT,
+            time=time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time())),
             reset=colorama.Style.RESET_ALL,
             msg=msg))
 
     @staticmethod
     def warn(msg):
-        sys.stderr.write(u'{warn}WARN:{reset} {msg}\n'.format(
+        sys.stderr.write(u'{warn}{time} WARN:{reset} {msg}\n'.format(
             warn=colorama.Fore.YELLOW + colorama.Style.BRIGHT,
+            time=time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time())),
             reset=colorama.Style.RESET_ALL,
             msg=msg))
