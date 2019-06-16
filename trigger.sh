392aab4a32299674c7053994e5f05d7070703a52
Jia Jia <jia.jia@zte.com.cn>
2019-06-16 16:24:04 +0800

    Enable debug mode in trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 trigger.py --mailer-config mailtrigger/config/mailer.json --scheduler-config mailtrigger/config/scheduler.json --trigger-config mailtrigger/config/trigger.json
+python3 trigger.py --debug --mailer-config mailtrigger/config/mailer.json --scheduler-config mailtrigger/config/scheduler.json --trigger-config mailtrigger/config/trigger.json
