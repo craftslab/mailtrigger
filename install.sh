19a0b1d81a16d57f7f523f166426a27375b1280a
Jia Jia <jia.jia@zte.com.cn>
2019-06-27 07:48:51 +0800

    Refactor code

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-pip3 install -U pywin32
-pip3 install -U PyInstaller
-pip3 install -Ur requirements.txt
+pip install -U pywin32
+pip install -U PyInstaller
+pip install -Ur requirements.txt
 
 pyinstaller --clean --name mailtrigger --upx-dir /path/to/upx -F trigger.py
--- trigger.sh
+++ trigger.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 trigger.py --debug --mailer-config mailtrigger/config/mailer.json --scheduler-config mailtrigger/config/scheduler.json --trigger-config mailtrigger/config/trigger.json
+python trigger.py --debug --mailer-config mailtrigger/config/mailer.json --scheduler-config mailtrigger/config/scheduler.json --trigger-config mailtrigger/config/trigger.json
