18856d574d1c4c99a4fa84e88b46273b5df2582d
Jia <49358172+craftslab@users.noreply.github.com>
2020-02-02 22:43:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 pip install -U pywin32
-pip install -U PyInstaller
+pip install -U pyinstaller
 pip install -Ur requirements.txt
 
 pyinstaller --clean --name mailtrigger --upx-dir /path/to/upx -F trigger.py
